420e1793a00b0f9fa34f6eb4190f7a5ec33fe407
Cameron de Bruyn <boertjie.seun@outlook.com>
2020-07-27 21:13:54 +0200

    New macOS Application
    #8 Develop add work Development 10m

---
+++ Swifty Quant/Swifty Quant/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Swifty Quant
+//
+//  Created by Cameron de Bruyn on 2020/07/27.
+//  Copyright © 2020 HINTON AI (PTY) LTD. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
